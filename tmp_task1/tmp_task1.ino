4d9164fc418f9d0be20666dfae34c681033ad4ea
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-01-29 23:37:14 +0700

    thêm bài buổi 2

---
+++ tmp_task1/tmp_task1.ino
@@ -0,0 +1,17 @@
+int sensorPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int reading = analogRead(sensorPin);
+
+  float voltage = reading * 5.0 / 1024.0;
+
+  float temp = voltage * 100.0;
+
+  Serial.println(temp);
+
+  delay(1000);
+}
